c89f29df9f4d564409207a218984e42d23f8c5a9
Ben Greanya <bengreanya@gmail.com>
2022-11-16 13:00:01 -0800

    created signup and signin functions to export from fetch

--- fetch-utils.js
+++ fetch-utils.js
@@ -9,9 +9,15 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
 
-export async function signInUser(email, password) {}
+export async function signInUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
 
 export async function checkAuth() {}
 
